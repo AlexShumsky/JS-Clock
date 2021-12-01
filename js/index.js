a744ae4e28ecdbcad1d391ef56db8d526bc2c3d7
AlexShumsky <sanya.shumskiy.95@mail.ru>
2021-12-01 23:18:02 +0300

    feat: add four time zones

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 const theme = document.querySelectorAll('.theme');
+const secondHands = document.querySelectorAll('.second-hand');
+const minuteHands = document.querySelectorAll('.minute-hand');
+const hourHands = document.querySelectorAll('.hour-hand');
+
 
 theme.forEach(el => el.addEventListener('click', function (e) {
 	removeActiveTheme();
@@ -15,3 +19,22 @@ function wrapperAddActiveTheme(el) {
 	const wrapper = document.querySelector('body');
 	(el.classList.contains('theme-d')) ? wrapper.classList.add('dark') : wrapper.classList.remove('dark');
 }
+function showTime() {
+	const date = new Date();
+	showSeconds(date.getSeconds());
+	showMinutes(date.getMinutes());
+	showHours(date.getHours(), date.getMinutes());
+}
+function showSeconds(seconds) {
+	const degree = (seconds / 60) * 360 + 90;
+	secondHands.forEach(secondHand => secondHand.style.transform = `rotate(${degree}deg)`);
+}
+function showMinutes(minutes) {
+	const degree = (minutes / 60) * 360 + 90;
+	minuteHands.forEach(minuteHand => minuteHand.style.transform = `rotate(${degree}deg)`);
+}
+function showHours(hours, mins) {
+	const degrees = [hours - 3, hours - 8, hours - 18, hours].map(hour => hour / 12 * 360 + 90 + (mins / 60) * 30);
+	hourHands.forEach((hourHand, city) => hourHand.style.transform = `rotate(${degrees[city]}deg)`);
+}
+setInterval(showTime, 1000);
